f21f8ef1324953649bcf3f09ec903bc432b8cbe3
fndjamoy <fabrice-edgar.ndjamoy-nzoudja@capgemini.com>
2023-12-08 11:33:26 +0100

    start baseline

---
+++ src/test/java/simpleTransaction/SimpleTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package simpleTransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
